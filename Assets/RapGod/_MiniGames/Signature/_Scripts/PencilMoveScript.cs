8aeab690c5baca74037b6626d2d0c47631876c52
VasuDev9 <vasup@crazylabs.com>
2022-06-30 15:41:30 +0530

    Signature Done

--- Assets/RapGod/_MiniGames/Signature/_Scripts/PencilMoveScript.cs
+++ Assets/RapGod/_MiniGames/Signature/_Scripts/PencilMoveScript.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class PencilMoveScript : MonoBehaviour
 {
+    public Vector3 screenPoint;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+        //transform.Rotate(0,1,0);
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        offset = Vector3.zero;
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+        //curPosition.z = curPosition.y;
+
+        //curPosition.y = 0.75f;
+        curPosition.y = transform.position.y;
+
+        transform.position = curPosition;
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("sign_cp"))
+        {
+            other.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
     }
 
     // Update is called once per frame
