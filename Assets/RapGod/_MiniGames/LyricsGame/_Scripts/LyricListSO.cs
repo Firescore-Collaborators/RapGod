591da0ec9782a82ed71f378df549b6b9f648f27e
Surya Prakash <suryap@tabtale.com>
2022-07-05 18:17:09 +0530

    Lyric so changes

--- Assets/RapGod/_MiniGames/LyricsGame/_Scripts/LyricListSO.cs
+++ Assets/RapGod/_MiniGames/LyricsGame/_Scripts/LyricListSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "LyricListSO", menuName = "RapGod/LyricList/LyricListSO")]
+[CreateAssetMenu(fileName = "LyricListSO", menuName = "RapBattle/LyricsGame/LyricListSO")]
 public class LyricListSO : ScriptableObject
 {
     public List<Lyrics_SO> lyricList = new List<Lyrics_SO>();
--- Assets/RapGod/_MiniGames/LyricsGame/_Scripts/Lyrics_SO.cs
+++ Assets/RapGod/_MiniGames/LyricsGame/_Scripts/Lyrics_SO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "LyricsGame", menuName = "RapBattle/LyricsGame", order = 51)]
+[CreateAssetMenu(fileName = "LyricsGame", menuName = "RapBattle/LyricsGame/Lyric", order = 51)]
 public class Lyrics_SO : ScriptableObject
 {
     public string Question, RightAnswer;
