627725c47ebeb8f2c0e0ffbb1d75448ee79aa4f4
Surya Prakash <suryap@tabtale.com>
2022-07-15 15:37:42 +0530

    received cherry 6

--- Assets/RapGod/_Scripts/SO/RapBattle/RapBattleDataSO.cs
+++ Assets/RapGod/_Scripts/SO/RapBattle/RapBattleDataSO.cs
@@ -2,14 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum RapAnimation
+public enum RapEndAnimation
 {
+    cherry,
     HandRaise,
 }
 public enum RapPose
 {
     layingDown,
-    WaveHipHop
+    Dying,
+    WaveHipHop,
+    cherry,
+
+}
+public enum rapCameras{
+    defaultCam = 0,
+    Camera1 = 1,
+    Camera2 = 2,
+    Camera3 = 3,
+    Camera4 = 4,
+    Camera5 = 5,
+    Camera6 = 6,
+    Camera7 = 7,
+    Camera8 = 8,
+    Camera9 = 9,
+    Camera10 = 10,
+    cherry,
+
+}
+
+public enum RapAnimation
+{
+    Rapping_Default,
+    Rapping1,
+    Rapping2,
+    Threatening,
+    StandingArgue1,
+    StandingArgue2,
+    StandingArgue3,
+    StandingArgue4,
+    Singing,
+    Defeated,
+    ArmStretching,
+    Dying,
+    Pointing,
+    SpellCast,
+    SurpriseUppercut,
+    WideArmSpellCast,
+    cherry,
+
+}
+
+[System.Serializable]
+public class RapAnimations
+{
+    public RapAnimation playerAnim;
+    public RapAnimation enemyAnim;
+}
+
+[System.Serializable]
+public class RapPoseData
+{
+    public RapPose playerSuccess;
+    public RapPose enemyFail;
+    public RapPose playerFail;
+    public RapPose enemySuccess;
+    private string cherry;
 }
 
 [CreateAssetMenu(fileName = "RapBattleDataSO", menuName = "RapBattle/RapBattle/RapBattleDataSO", order = 1)]
@@ -19,9 +77,13 @@ public class RapBattleDataSO : ScriptableObject
     public EnvironmentSO environment;
     public RapBattleLyricSO rapBattleLyricSO;
     public InputSequenceSO inputSequence;
-    public RapAnimation rapAnimation;
-    public RapPose rapPose;
+    public RapEndAnimation rapEndAnimation;
+    public RapPoseData rapPoses;
+    private string cherry;
+    public List<RapAnimations> rapAnimations = new List<RapAnimations>();
+    public List<rapCameras> rapCameras = new List<rapCameras>();
     public string punchLine;
+    public string youtubeVideoTitle = "LATEST RAP SONG RELEASED";
     public float tapSmashLimit = 20f;
     public float maxAnimationSpeed = 3f;
 
