fe7b12f4d1f6c4dfb4d431d886190be175da3563
VasuDev9 <vasup@crazylabs.com>
2022-07-25 21:10:07 +0530

    haircut done

--- Assets/RapGod/_MiniGames/CharacterLook/_Scripts/HaircutScript.cs
+++ Assets/RapGod/_MiniGames/CharacterLook/_Scripts/HaircutScript.cs
@@ -1,24 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class HaircutScript : MonoBehaviour
 {
-    public GameObject Girl, TrimmerBlack;
+    public GameObject Girl, TrimmerBlack, Chair, GirlChat, ReferenceImage, CameraFinal;
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("StopAnim", 1);
+        
+        Invoke("StopAnim", 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.M))
+        {
+            StartCoroutine(Entry());
+            ReferenceImage.SetActive(false);
+            CameraFinal.SetActive(true);
+        }
     }
 
     public void StopAnim()
     {
         Girl.GetComponent<Animator>().enabled = false;
+    }
+
+    IEnumerator Entry()
+    {
+        yield return new WaitForSeconds(1);
+        Chair.transform.DORotate(new Vector3(0, -130, 0), 2);
+
+        yield return new WaitForSeconds(2);
+        GirlChat.SetActive(true);
+        Girl.GetComponent<Animator>().enabled = true;
+        Girl.GetComponent<Animator>().SetTrigger("Angry");
+
+
     }
 }
