aa733e9faf8a8a8958cb6153692c6584afe39cb8
VasuDev9 <vasup@crazylabs.com>
2022-07-26 21:42:20 +0530

    Ear Piercing done!

--- Assets/RapGod/_MiniGames/CharacterLook/_Scripts/HaircutScript.cs
+++ Assets/RapGod/_MiniGames/CharacterLook/_Scripts/HaircutScript.cs
@@ -5,11 +5,12 @@ using DG.Tweening;
 
 public class HaircutScript : MonoBehaviour
 {
-    public GameObject Girl, TrimmerBlack, Chair, GirlChat, ReferenceImage, CameraFinal;
+    public GameObject Girl, TrimmerBlack, Chair, GirlChat, ReferenceImage, CameraFinal, Piston1, Piston2, 
+        topRing, sideRing, Eyering, ShakePanel, indicatorRing1, indicatorRing2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
         Invoke("StopAnim", 0.5f);
     }
 
@@ -22,6 +23,47 @@ public class HaircutScript : MonoBehaviour
             ReferenceImage.SetActive(false);
             CameraFinal.SetActive(true);
         }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Piston1.GetComponent<Animator>().enabled = true;
+            Invoke("ShakeScreen", 0.5f);
+            Invoke("ShakeOff", 4);
+            Invoke("ShowJwel1", 2);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Invoke("ShakeScreen", 0.5f);
+            Piston2.GetComponent<Animator>().enabled = true;
+            Invoke("ShowJwel2", 2);
+            Invoke("ShakeOff", 4);
+
+        }
+    }
+
+    public void ShakeScreen()
+    {
+        ShakePanel.SetActive(true);
+    }
+    public void ShakeOff()
+    {
+        ShakePanel.SetActive(false);
+    }
+
+    public void ShowJwel1()
+    {
+        topRing.SetActive(true);
+        indicatorRing1.SetActive(false);
+    }
+    public void ShowJwel2()
+    {
+        indicatorRing2.SetActive(false);
+        sideRing.SetActive(true);
+    }
+    public void ShowJwel3()
+    {
+        Eyering.SetActive(true);
     }
 
     public void StopAnim()
@@ -38,7 +80,5 @@ public class HaircutScript : MonoBehaviour
         GirlChat.SetActive(true);
         Girl.GetComponent<Animator>().enabled = true;
         Girl.GetComponent<Animator>().SetTrigger("Angry");
-
-
     }
 }
